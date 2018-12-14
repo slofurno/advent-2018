2da9b8258fa2d609b4a2316f6e435c00fbccfae6
Steve LoFurno <slofurno@gmail.com>
2018-12-13 19:12:04 -0500

    wip

--- 12/index.js
+++ 12/index.js
@@ -2,28 +2,59 @@ var fs = require('fs')
 
 var input = fs.readFileSync("./input.txt", "utf-8").split("\n").slice(0,-1)
 
-var state = input[0].split(": ")[1]
+var pos = input[0].split(": ")[1].split("").map(x => x === "#" ? 1 : 0)
+var neg = []
 
-var rules = {}
-input.slice(2).map(x => x.split(" => ")).forEach(([k,v]) => rules[k] = v)
+var rules = ".".repeat(32).split("").map(x => 0)
+input.slice(2).map(x => x.split(" => ")).forEach(([k,v]) => {
+  var n = 0
+  k.split("").map(x => x === "#" ? 1 : 0).forEach((x,i) => {
+    n = n * 2 + x
+  })
 
-let gens = 50000000000
+  rules[n] = v === "#" ? 1 : 0
+})
+
+//console.log(rules)
+
+let gens = 50000000
 
 for (var k = 0; k < gens; k++) {
-  //console.log(state)
-  state = "...." + state + "...."
-  let next = state.split("").slice(2,-2)
-  for (var i = 0; i < next.length; i++) {
-    next[i] = rules[state.slice(i, i+5)]
+//  console.log(JSON.stringify(neg))
+ // console.log(JSON.stringify(pos))
+  var n = 0
+  var gg = pos.length + 5
+  for (var i = -neg.length; i < gg; i++) {
+    n = (n << 1) & 31
+    if (i < 0) {
+      n |= neg[-i] === 1 ? 1 : 0
+    } else {
+      n |= pos[i] === 1 ? 1 : 0
+    }
+
+    var p = i - 2
+    if (p < 0) {
+      if (neg[-p] === 1 || rules[n] === 1) {
+        neg[-p] = rules[n]
+      }
+    } else {
+      if (pos[p] === 1 || rules[n] === 1) {
+        pos[p] = rules[n]
+      }
+    }
   }
-  state = next.join("")
 }
 
 var sum = 0
-for (var i = 0, j = -1 * gens; i < state.length; i++, j++) {
-  if (state[i] == "#") {
-    sum += j
+for (var i = 0; i < neg.length; i++) {
+  if (neg[i] === 1) {
+    sum -= i
   }
 }
 
+for (var i = 0; i < pos.length; i++) {
+  if (pos[i] === 1) {
+    sum += i
+  }
+}
 console.log(sum)
